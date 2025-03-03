90638b006ccdf80ac910a2f80f8c013704fcbb73
Connor Powers <20464743+connor-powers@users.noreply.github.com>
2025-03-03 14:49:20 -0800

    First commit, adding general skeleton structure of files, implementing enough to get basic map generation and visualization working
    Added ManualRewardMap class and site_obj struct, as well as an example of a basic workflow to manually add sites of interest by coordinates then generate the resulting reward map
    Also don't think this project needs a license

---
+++ include/ManualRewardMap.h
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
+
+struct site_obj
+{
+    std::pair<double,double> coordinates;
+    double reward_val;
+};
+
+
+class ManualRewardMap
+{
+    private:
+
+        double min_site_x_coord_;
+        double max_site_x_coord_;
+        double min_site_y_coord_;
+        double max_site_y_coord_;
+        double max_reward_val_={0};
+
+
+
+        std::pair<size_t,size_t> coords_to_map_indices(std::pair<double,double> input_coords);
+    public:
+        double min_x_={0};
+        double max_x_={0};
+        double min_y_={0};
+        double max_y_={0};
+
+        size_t map_side_length_x_={100};
+        size_t map_side_length_y_={100};
+
+        MatrixXd map_;
+        std::vector<site_obj> list_of_sites_={};
+        std::pair<double,double> starting_position_={};
+
+        ManualRewardMap(std::vector<site_obj> input_list_of_sites={}){
+            if (input_list_of_sites.size()>0){
+                for (site_obj site : input_list_of_sites){
+                    double site_x=site.coordinates.first;
+                    double site_y=site.coordinates.second;
+                    if (site_x<min_site_x_coord_){
+                        min_site_x_coord_=site_x;
+                    }
+
+                    if (site_x>max_site_x_coord_){
+                        max_site_x_coord_=site_x;
+                    }
+
+                    if (site_y<min_site_y_coord_){
+                        min_site_y_coord_=site_y;
+                    }
+
+                    if (site_y>max_site_y_coord_){
+                        max_site_y_coord_=site_y;
+                    }
+                    if (site.reward_val>max_reward_val_){
+                        max_reward_val_=site.reward_val;
+                    }
+
+                    list_of_sites_.push_back(site);
+                }
+            }
+            else {
+                min_site_x_coord_=min_x_;
+                min_site_y_coord_=min_y_;
+                max_site_x_coord_=max_x_;
+                max_site_y_coord_=max_y_;
+            }
+        }
+    
+        void add_site(site_obj input_site){
+            list_of_sites_.push_back(input_site);
+        }
+
+        void set_initial_position(std::pair<double,double> input_position){
+            starting_position_=input_position;
+        }
+
+        void set_map_side_length_x(size_t input_length_x){
+            map_side_length_x_=input_length_x;
+        }
+
+        void set_map_side_length_y(size_t input_length_y){
+            map_side_length_y_=input_length_y;
+        }
+
+        void generate_map();
+
+        void draw_map();
+
+
+
+};
+
