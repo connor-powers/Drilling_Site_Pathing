cc2edb4eec4564716d35b8e27709b2f7c5ac5a57
Connor Powers <20464743+connor-powers@users.noreply.github.com>
2025-04-21 17:17:15 -0700

    Running clang-format on project files

--- include/RandomRewardMap.h
+++ include/RandomRewardMap.h
@@ -1,88 +1,93 @@
-#include <iostream>
 #include <Eigen/Dense>
+#include <iostream>
 #include <random>
+
 #include "utils.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+class RandomRewardMap {
+ private:
+  std::vector<site> list_of_sites_ = {};
 
-class RandomRewardMap
-{
-    private:
+  std::pair<size_t, size_t> coords_to_map_indices(
+      std::pair<double, double> input_coords);
 
+ public:
+  std::pair<double, double> starting_position_;
+  double min_x_ = {0};
+  double max_x_ = {0};
+  double min_y_ = {0};
+  double max_y_ = {0};
+  double reward_range_min_ = {0};
+  double reward_range_max_ = {1};
 
-        std::vector<site> list_of_sites_={};
+  size_t map_side_length_x_ = {100};
+  size_t map_side_length_y_ = {100};
 
-        std::pair<size_t,size_t> coords_to_map_indices(std::pair<double,double> input_coords);
-    public:
-        std::pair<double,double> starting_position_;
-        double min_x_={0};
-        double max_x_={0};
-        double min_y_={0};
-        double max_y_={0};
-        double reward_range_min_={0};
-        double reward_range_max_={1};
+  MatrixXd map_;
 
-        size_t map_side_length_x_={100};
-        size_t map_side_length_y_={100};
+  RandomRewardMap(double map_side_length_x, double map_side_length_y,
+                  double min_x, double max_x, double min_y, double max_y,
+                  double reward_range_min, double reward_range_max) {
+    map_side_length_x_ = map_side_length_x;
+    map_side_length_y_ = map_side_length_y;
+    min_x_ = min_x;
+    min_y_ = min_y;
+    max_x_ = max_x;
+    max_y_ = max_y;
+    reward_range_min_ = reward_range_min;
+    reward_range_max_ = reward_range_max;
+    starting_position_.first = 0;
+    starting_position_.second = 0;
 
-        MatrixXd map_;
+    std::random_device test_random_device;
+    std::mt19937 engine(test_random_device());
+    std::uniform_real_distribution<> reward_val_distribution(reward_range_min_,
+                                                             reward_range_max_);
 
+    // map_=MatrixXd::Random(map_side_length_y_,map_side_length_x_);
+    map_ = MatrixXd::NullaryExpr(map_side_length_y_, map_side_length_x_,
+                                 [&engine, &reward_val_distribution]() {
+                                   return reward_val_distribution(engine);
+                                 });
 
-        RandomRewardMap(double map_side_length_x,double map_side_length_y,double min_x,double max_x,double min_y, double max_y,double reward_range_min, double reward_range_max){
-            map_side_length_x_=map_side_length_x;
-            map_side_length_y_=map_side_length_y;
-            min_x_=min_x;
-            min_y_=min_y;
-            max_x_=max_x;
-            max_y_=max_y;
-            reward_range_min_=reward_range_min;
-            reward_range_max_=reward_range_max;
-            starting_position_.first=0;
-            starting_position_.second=0;
+    // rescale to desired range since by default Random generates floats between
+    // -1 and 1
+    //  map_*=((reward_range_max_-reward_range_min_)/2); //because initially has
+    //  a range of 2 from going between -1 and 1
 
-            std::random_device test_random_device;
-            std::mt19937 engine(test_random_device());
-            std::uniform_real_distribution<> reward_val_distribution(reward_range_min_,reward_range_max_);
+    // MatrixXd
+    // tmp_mat=MatrixXd::Constant(map_side_length_y_,map_side_length_x_,reward_range_min_+((reward_range_max_-reward_range_min_)/2));
+    // map_+=tmp_mat;
+  }
 
+  void set_initial_position(std::pair<double, double> input_position) {
+    starting_position_ = input_position;
+  }
 
-            // map_=MatrixXd::Random(map_side_length_y_,map_side_length_x_);
-            map_=MatrixXd::NullaryExpr(map_side_length_y_,map_side_length_x_,[&engine,&reward_val_distribution](){return reward_val_distribution(engine);});
+  void identify_potential_sites(int input_num_sites,
+                                double input_site_reward_val_threshold = 0.5);
+  void identify_potential_sites(double input_site_reward_val_threshold = 0.5);
 
-            //rescale to desired range since by default Random generates floats between -1 and 1
-            // map_*=((reward_range_max_-reward_range_min_)/2); //because initially has a range of 2 from going between -1 and 1
+  void draw_map();
 
-            // MatrixXd tmp_mat=MatrixXd::Constant(map_side_length_y_,map_side_length_x_,reward_range_min_+((reward_range_max_-reward_range_min_)/2));
-            // map_+=tmp_mat;
+  void draw_map_with_paths(const std::vector<site>,
+                           std::string input_path_type = "",
+                           double associated_distance_weight = 0);
+  // Current options for input_path_type: "DescendingPriority" if the path being
+  // plotted was generated with the descending priority method, or "Weighted_NN"
+  //if the path being plotted was generated with the distance-weighted
+  //nearest-neighbor method.
+  //  If this is the case, pass the associated distance weight as the third
+  //  argument.
 
+  std::pair<std::vector<site>, double> generate_paths_distance_weighted_NN(
+      const double distance_weight);
 
-        }
-    
+  void plot_NN_total_distance_swept_distance_weight(
+      double weight_increment, size_t num_weight_increments);
 
-        void set_initial_position(std::pair<double,double> input_position){
-            starting_position_=input_position;
-        }
-
-        void identify_potential_sites(int input_num_sites, double input_site_reward_val_threshold=0.5);
-        void identify_potential_sites(double input_site_reward_val_threshold=0.5);
-
-
-        void draw_map();
-
-
-        void draw_map_with_paths(const std::vector<site>, std::string input_path_type="", double associated_distance_weight=0);
-        //Current options for input_path_type: "DescendingPriority" if the path being plotted was generated with the descending priority method, or 
-        //"Weighted_NN" if the path being plotted was generated with the distance-weighted nearest-neighbor method. 
-        // If this is the case, pass the associated distance weight as the third argument.
-
-        std::pair<std::vector<site>,double> generate_paths_distance_weighted_NN(const double distance_weight);
-
-        void plot_NN_total_distance_swept_distance_weight(double weight_increment,size_t num_weight_increments);
-
-        std::vector<site> get_list_of_sites(){
-            return list_of_sites_;
-        }
-
+  std::vector<site> get_list_of_sites() { return list_of_sites_; }
 };
-
--- src/random_map_demo.cpp
+++ src/random_map_demo.cpp
@@ -1,72 +1,82 @@
 #include <iostream>
+
 #include "RandomRewardMap.h"
 
+int main() {
+  // First, set parameters of the random map
+  double map_side_length_x = 21;  // in pixels
+  double map_side_length_y = 21;
+  double min_x = 0;
+  double max_x = 1;
+  double min_y = 0;
+  double max_y = 1;
+  double reward_range_min = 0;
+  double reward_range_max = 3;
+  std::pair<double, double> initial_position = {0, 0};
+  RandomRewardMap example_map(map_side_length_x, map_side_length_y, min_x,
+                              max_x, min_y, max_y, reward_range_min,
+                              reward_range_max);
 
-int main(){
+  // uncomment this to see map drawn before any kind of pathing takes place
+  //  example_map.draw_map();
 
-    //First, set parameters of the random map
-    double map_side_length_x=21; //in pixels
-    double map_side_length_y=21;
-    double min_x=0;
-    double max_x=1;
-    double min_y=0;
-    double max_y=1;
-    double reward_range_min=0;
-    double reward_range_max=3;
-    std::pair<double,double> initial_position={0,0};
-    RandomRewardMap example_map(map_side_length_x,map_side_length_y,
-        min_x,max_x,
-        min_y,max_y,
-        reward_range_min,reward_range_max);
+  // Identify potentially interesting sites
+  double site_reward_val_threshold;
 
-    //uncomment this to see map drawn before any kind of pathing takes place
-    // example_map.draw_map();
+  // uncomment this block to see example of limiting the number of identified
+  // sites
+  //  int num_sites=5;
+  //  site_reward_val_threshold=0.65;
+  //  example_map.identify_potential_sites(num_sites,
+  //  site_reward_val_threshold);
+  //  //Since list_of_sites_ is already sorted in decreasing order of reward
+  //  val,
+  //  //can directly pass it here to get descending priority method path
+  //  std::vector<site>
+  //  list_of_identified_sites=example_map.get_list_of_sites();
+  //  example_map.draw_map_with_paths(list_of_identified_sites,"DescendingPriority");
 
-    //Identify potentially interesting sites
-    double site_reward_val_threshold;
+  site_reward_val_threshold = 2.7;
+  // When no num_sites is passed to identify_potential_sites, it includes all
+  // sites whose reward vals meet or exceed the threshold
+  example_map.identify_potential_sites(site_reward_val_threshold);
 
-    //uncomment this block to see example of limiting the number of identified sites
-    // int num_sites=5;
-    // site_reward_val_threshold=0.65;
-    // example_map.identify_potential_sites(num_sites, site_reward_val_threshold);
-    // //Since list_of_sites_ is already sorted in decreasing order of reward val, 
-    // //can directly pass it here to get descending priority method path
-    // std::vector<site> list_of_identified_sites=example_map.get_list_of_sites();
-    // example_map.draw_map_with_paths(list_of_identified_sites,"DescendingPriority"); 
+  std::vector<site> new_list_of_identified_sites =
+      example_map.get_list_of_sites();
+  example_map.draw_map_with_paths(new_list_of_identified_sites,
+                                  "DescendingPriority");
+  double descending_priority_distance = calculate_total_distance_from_sequence(
+      example_map.starting_position_, new_list_of_identified_sites);
+  std::cout << "Total distance of descending priority method: "
+            << descending_priority_distance << "\n";
 
+  // Now I'll demonstrate paths using a nearest-neighbor method with varying
+  // distance weight
 
-    site_reward_val_threshold=2.7;
-    //When no num_sites is passed to identify_potential_sites, it includes all sites whose reward vals meet or exceed the threshold
-    example_map.identify_potential_sites(site_reward_val_threshold);
+  double distance_weight_1 = 0.1;
+  std::pair<std::vector<site>, double> NN_paths_and_distance_1 =
+      example_map.generate_paths_distance_weighted_NN(distance_weight_1);
+  std::vector<site> NN_paths_1 = NN_paths_and_distance_1.first;
+  double NN_distance_1 = NN_paths_and_distance_1.second;
+  example_map.draw_map_with_paths(NN_paths_1, "Weighted_NN", distance_weight_1);
+  std::cout << "Total distance of NN method with distance weight = "
+            << distance_weight_1 << ": " << NN_distance_1 << "\n";
 
-    std::vector<site> new_list_of_identified_sites=example_map.get_list_of_sites();
-    example_map.draw_map_with_paths(new_list_of_identified_sites,"DescendingPriority"); 
-    double descending_priority_distance=calculate_total_distance_from_sequence(example_map.starting_position_,new_list_of_identified_sites);
-    std::cout << "Total distance of descending priority method: " << descending_priority_distance<< "\n";
+  double distance_weight_2 = 100;
+  std::pair<std::vector<site>, double> NN_paths_and_distance_2 =
+      example_map.generate_paths_distance_weighted_NN(distance_weight_2);
+  std::vector<site> NN_paths_2 = NN_paths_and_distance_2.first;
+  double NN_distance_2 = NN_paths_and_distance_2.second;
+  example_map.draw_map_with_paths(NN_paths_2, "Weighted_NN", distance_weight_2);
+  std::cout << "Total distance of NN method with distance weight = "
+            << distance_weight_2 << ": " << NN_distance_2 << "\n";
 
-    //Now I'll demonstrate paths using a nearest-neighbor method with varying distance weight
+  // Let's try sweeping this distance_weight and see if there's a noticeable
+  // minimum of the total distance
+  double weight_increment = 0.001;
+  size_t num_weight_increments = 10000;
+  example_map.plot_NN_total_distance_swept_distance_weight(
+      weight_increment, num_weight_increments);
 
-
-    double distance_weight_1=0.1;
-    std::pair<std::vector<site>,double>  NN_paths_and_distance_1=example_map.generate_paths_distance_weighted_NN(distance_weight_1);
-    std::vector<site> NN_paths_1=NN_paths_and_distance_1.first;
-    double NN_distance_1=NN_paths_and_distance_1.second;
-    example_map.draw_map_with_paths(NN_paths_1,"Weighted_NN",distance_weight_1);
-    std::cout << "Total distance of NN method with distance weight = " << distance_weight_1 << ": " << NN_distance_1 << "\n";
-    
-
-    double distance_weight_2=100;
-    std::pair<std::vector<site>,double>  NN_paths_and_distance_2=example_map.generate_paths_distance_weighted_NN(distance_weight_2);
-    std::vector<site> NN_paths_2=NN_paths_and_distance_2.first;
-    double NN_distance_2=NN_paths_and_distance_2.second;
-    example_map.draw_map_with_paths(NN_paths_2,"Weighted_NN",distance_weight_2);
-    std::cout << "Total distance of NN method with distance weight = " << distance_weight_2 << ": " << NN_distance_2 << "\n";
-
-    //Let's try sweeping this distance_weight and see if there's a noticeable minimum of the total distance
-    double weight_increment=0.001;
-    size_t num_weight_increments=10000;
-    example_map.plot_NN_total_distance_swept_distance_weight(weight_increment,num_weight_increments);
-    
-
-    return 0;
+  return 0;
 }
